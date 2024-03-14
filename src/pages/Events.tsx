28957f6910fe8ed069d525300963f3c115ca0748
seniorsardorbek <sardorbekmusilman@gmail.com>
2024-03-14 22:09:32 +0500

    hello

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -18,7 +18,7 @@ const options = [
     { value: '1500', label: '1500' },
     { value: '2000', label: '2000' },
     { value: '5000', label: '5000' },
-    { value: '7000', label: '7000' },
+    { value: '7000', label: '7000' }
 ];
 function Events () {
     const dispatch = useDispatch();
@@ -34,7 +34,7 @@ function Events () {
     const [page, setPage] = useState<number>(0);
     const { token } = useSelector((state: IRootState) => state.data);
 
-    const header = ['_id', 'level', 'volume', 'date_in_ms', 'signal', 'updated_at', 'created_at', 'serie' , "name"];
+    const header = ['_id', 'level', 'volume', 'date_in_ms', 'signal', 'updated_at', 'created_at', 'serie', 'name'];
     useEffect(() => {
         dispatch(setPageTitle('Constructor'));
         getData({ url: '/regions', setData: setRegions, token });
@@ -65,13 +65,13 @@ function Events () {
             token
         });
     };
-    const handleDownloadExcel = () =>{
+    const handleDownloadExcel = () => {
         const handleExel: EventFaceHandelExel[] = events.data.map((el: EventFace) => {
             const { device, ...data } = el;
             return {
                 ...data,
-                serie: device.serie ,
-                name : device.name
+                serie: device.serie,
+                name: device.name
             };
         });
         downloadExcel({
@@ -82,16 +82,16 @@ function Events () {
                 body: handleExel
             }
         });
-    }
+    };
 
     const handleDownloadAll = () => {
-        const fileDownloadUrl = `https://back2.livewater.uz/basedata/xlsx?${from?`&filter[start]=${from}`:''}${to?`&filter[end]=${to}`:''}${device?`&filter[device]=${device}`:''}${
+        const fileDownloadUrl = `https://back2.livewater.uz/basedata/xlsx?${from ? `&filter[start]=${from}` : ''}${to ? `&filter[end]=${to}` : ''}${device ? `&filter[device]=${device}` : ''}${
             data?.region ? `&filter[region]=${data.region}` : ''
         }${data?.limit ? `&page[limit]=${data.limit}` : ''}`;
         axios({
             url: fileDownloadUrl,
             method: 'GET',
-            responseType: 'blob', 
+            responseType: 'blob',
             onDownloadProgress: e => {
                 const newProgress = Math.round((e.loaded * 100) / e.total!);
                 setProgress(newProgress);
@@ -118,7 +118,7 @@ function Events () {
             [e.target.name]: e.target.value
         }));
     };
-    console.log(events.offset+1);
+    console.log(events.offset + 1);
     return (
         <>
             <ul className='flex space-x-2 rtl:space-x-reverse'>
@@ -155,11 +155,7 @@ function Events () {
                             </svg>
                             Sahifadan yuklash
                         </button>
-                        <button
-                        disabled={progress !==0}
-                        onClick={handleDownloadAll}
-                            className='btn btn-primary btn-sm m-1'
-                        >
+                        <button disabled={progress !== 0} onClick={handleDownloadAll} className='btn btn-primary btn-sm m-1'>
                             <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' className='w-5 h-5 ltr:mr-2 rtl:ml-2'>
                                 <path
                                     d='M15.3929 4.05365L14.8912 4.61112L15.3929 4.05365ZM19.3517 7.61654L18.85 8.17402L19.3517 7.61654ZM21.654 10.1541L20.9689 10.4592V10.4592L21.654 10.1541ZM3.17157 20.8284L3.7019 20.2981H3.7019L3.17157 20.8284ZM20.8284 20.8284L20.2981 20.2981L20.2981 20.2981L20.8284 20.8284ZM14 21.25H10V22.75H14V21.25ZM2.75 14V10H1.25V14H2.75ZM21.25 13.5629V14H22.75V13.5629H21.25ZM14.8912 4.61112L18.85 8.17402L19.8534 7.05907L15.8947 3.49618L14.8912 4.61112ZM22.75 13.5629C22.75 11.8745 22.7651 10.8055 22.3391 9.84897L20.9689 10.4592C21.2349 11.0565 21.25 11.742 21.25 13.5629H22.75ZM18.85 8.17402C20.2034 9.3921 20.7029 9.86199 20.9689 10.4592L22.3391 9.84897C21.9131 8.89241 21.1084 8.18853 19.8534 7.05907L18.85 8.17402ZM10.0298 2.75C11.6116 2.75 12.2085 2.76158 12.7405 2.96573L13.2779 1.5653C12.4261 1.23842 11.498 1.25 10.0298 1.25V2.75ZM15.8947 3.49618C14.8087 2.51878 14.1297 1.89214 13.2779 1.5653L12.7405 2.96573C13.2727 3.16993 13.7215 3.55836 14.8912 4.61112L15.8947 3.49618ZM10 21.25C8.09318 21.25 6.73851 21.2484 5.71085 21.1102C4.70476 20.975 4.12511 20.7213 3.7019 20.2981L2.64124 21.3588C3.38961 22.1071 4.33855 22.4392 5.51098 22.5969C6.66182 22.7516 8.13558 22.75 10 22.75V21.25ZM1.25 14C1.25 15.8644 1.24841 17.3382 1.40313 18.489C1.56076 19.6614 1.89288 20.6104 2.64124 21.3588L3.7019 20.2981C3.27869 19.8749 3.02502 19.2952 2.88976 18.2892C2.75159 17.2615 2.75 15.9068 2.75 14H1.25ZM14 22.75C15.8644 22.75 17.3382 22.7516 18.489 22.5969C19.6614 22.4392 20.6104 22.1071 21.3588 21.3588L20.2981 20.2981C19.8749 20.7213 19.2952 20.975 18.2892 21.1102C17.2615 21.2484 15.9068 21.25 14 21.25V22.75ZM21.25 14C21.25 15.9068 21.2484 17.2615 21.1102 18.2892C20.975 19.2952 20.7213 19.8749 20.2981 20.2981L21.3588 21.3588C22.1071 20.6104 22.4392 19.6614 22.5969 18.489C22.7516 17.3382 22.75 15.8644 22.75 14H21.25ZM2.75 10C2.75 8.09318 2.75159 6.73851 2.88976 5.71085C3.02502 4.70476 3.27869 4.12511 3.7019 3.7019L2.64124 2.64124C1.89288 3.38961 1.56076 4.33855 1.40313 5.51098C1.24841 6.66182 1.25 8.13558 1.25 10H2.75ZM10.0298 1.25C8.15538 1.25 6.67442 1.24842 5.51887 1.40307C4.34232 1.56054 3.39019 1.8923 2.64124 2.64124L3.7019 3.7019C4.12453 3.27928 4.70596 3.02525 5.71785 2.88982C6.75075 2.75158 8.11311 2.75 10.0298 2.75V1.25Z'
@@ -168,7 +164,7 @@ function Events () {
                                 <path opacity='0.5' d='M13 2.5V5C13 7.35702 13 8.53553 13.7322 9.26777C14.4645 10 15.643 10 18 10H22' stroke='currentColor' strokeWidth='1.5' />
                                 <path opacity='0.5' d='M7 14L6 15L7 16M11.5 16L12.5 17L11.5 18M10 14L8.5 18' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
                             </svg>
-                           {progress === 0 ?' Barchasini yuklash' :`Yuklanmoqda...${progress}%`}
+                            {progress === 0 ? ' Barchasini yuklash' : `Yuklanmoqda...${progress}%`}
                         </button>
                     </div>
                 </div>
@@ -186,18 +182,20 @@ function Events () {
                             onChange={date3 => setDate3(date3)}
                         />
                         <div>
-                            <label className='inline-flex justify-between items-center'>
+                            <label className='inline-flex   items-start '>
                                 <input onChange={e => handleChange(e)} type='radio' name='region' value={''} className='form-radio outline-success' />
                                 <span>Hammasi</span>
+                                
                             </label>
-                            {regions.data.map((el, i) => (
-                                <label key={i} className='inline-flex justify-between items-center'>
-                                    <input onChange={e => handleChange(e)} type='radio' name='region' value={el._id} className='form-radio outline-success' />
-                                    <span>{el.name}</span>
-                                </label>
-                            ))}
+                            <div className="flex flex-col items-start max-h-[80%]">
+                                {regions.data.map((el, i) => (
+                                    <label key={i} className='inline-flex  justify-between items-center'>
+                                        <input onChange={e => handleChange(e)} type='radio' name='region' value={el._id} className='form-radio outline-success' />
+                                        <span>{el.name}</span>
+                                    </label>
+                                ))}</div>
                         </div>
-                        <select className='form-input flex gap-2' onChange={e => setDevice(e.target.value)}>
+                        <select className='form-input flex  gap-2' onChange={e => setDevice(e.target.value)}>
                             <option value={''}>Seriya</option>
                             {devices.data.map((el, i) => (
                                 <option value={el._id} key={i}>
@@ -205,7 +203,7 @@ function Events () {
                                 </option>
                             ))}
                         </select>
-                        <div className='mb-5 flex items-center justify-center w-[80%] h-4' id='limit_tagging'>
+                        <div className='mb-5 flex items-center justify-center h-5  w-full' id='limit_tagging'>
                             {loading ? <Miniloader /> : ''}
                         </div>
                         <button type='submit' className='btn btn-outline-primary w-full '>
@@ -234,7 +232,7 @@ function Events () {
                                 {events.data.map((data, i) => {
                                     return (
                                         <tr key={data._id}>
-                                           <td>{((events.limit *  events.offset) + (i+1))}</td>
+                                            <td>{events.limit * events.offset + (i + 1)}</td>
                                             <td>
                                                 <div className='whitespace-nowrap text-xs'>{data?.device?.serie}</div>
                                             </td>
